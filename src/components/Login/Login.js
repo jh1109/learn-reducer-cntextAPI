b4d76418a5882e1d2047bfe63511488859c93735
Juhee Noh <nohjuhee227@gmail.com>
2023-06-14 14:50:55 +0900

    learn. useReducer 사용하여 비밀번호 상태관리(정답)(127)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,10 +17,10 @@ const emailReducer = (state, action) => {
 };
 const passwordReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
-    return { value: action.val, isValid: action.val.trim() > 6 };
+    return { value: action.val, isValid: action.val.trim().length > 6 };
   }
   if (action.type === "USER_BLUR") {
-    return { value: state.value, isValid: state.value.trim() > 6 };
+    return { value: state.value, isValid: state.value.trim().length > 6 };
   }
   return { value: "", isValid: false };
 };
