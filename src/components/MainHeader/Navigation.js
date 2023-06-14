36b8d34bffc8f63c451c9aaa3c96915feb4beba1
Juhee Noh <nohjuhee227@gmail.com>
2023-06-14 16:53:38 +0900

    learn. useContext() 이용하여 로그인 상태 관리하기(132)

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,37 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+  // context를 리스닝하게 해주는 Hook
+  const ctx = useContext(AuthContext);
+  // console.log(ctx);
   return (
-    <AuthContext.Consumer>
-      {/* 사용할 곳에 감싼다.(소비) */}
-      {(ctx) => {
-        // console.log(ctx); // context의 객체
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
